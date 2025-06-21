91127fe2a44632ca27c66b25f3b88e31a69c67a1
Yavuz Oktay <yavuzdark34@gmail.com>
2025-06-21 14:44:50 +0300

    Update RightInfoCard.jsx

--- src/frontend/components/RightInfoCard.jsx
+++ src/frontend/components/RightInfoCard.jsx
@@ -55,7 +55,7 @@ const RightInfoCard = ({ activitySelect, chooseLocation, chooseFlightType, choos
         };
 
         try {
-            const response = await axios.post('http://localhost:3002/api/createBooking', bookingData);
+            const response = await axios.post('http://13.221.13.241:3002/api/createBooking', bookingData);
             if (response.data.success) {
                 alert('Booking successful! Booking ID: ' + response.data.bookingId);
                 // Optionally, reset the form or redirect
