b5baafb7097e18aeaf304c418ef69344c2385a6c
Yavuz OKTAY <yavuzoktay@MacBook-Pro-8.local>
2025-07-06 13:32:19 +0100

    Update: book frontend changes

--- src/frontend/api/useBookingDataAll.js
+++ src/frontend/api/useBookingDataAll.js
@@ -9,7 +9,7 @@ const useBooking = () => {
     useEffect(() => {
         const fetchFlights = async () => {
             try {
-                const response = await axios.get(`http:/3.92.209.28:3002/api/getAllBookingData`);
+                const response = await axios.get(`http://44.203.65.48/:3000/api/getAllBookingData`);
                 setBooking(response.data.data);
             } catch (error) {
                 // console.error("Error fetching flights:", error);
